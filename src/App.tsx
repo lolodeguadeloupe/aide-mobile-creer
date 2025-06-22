1763fc764e57d44400b7c985f85a20f21c576644
Luce laurent <cmoinster@gmail.com>
2025-06-22 20:18:18 +0200

    Coolify update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "@/contexts/AuthContext";
+import { Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import AccommodationsScreen from "./components/AccommodationsScreen";
@@ -16,32 +12,24 @@ import ActivitiesScreen from "./components/ActivitiesScreen";
 import CarRentalsScreen from "./components/CarRentalsScreen";
 import PWAInstallPrompt from "./components/PWAInstallPrompt";
 
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <AuthProvider>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/accommodations" element={<AccommodationsScreen />} />
-            <Route path="/restaurants" element={<RestaurantsScreen />} />
-            <Route path="/concerts" element={<ConcertsScreen />} />
-            <Route path="/nightlife" element={<NightlifeScreen />} />
-            <Route path="/loisirs" element={<LoisirsScreen />} />
-            <Route path="/activities" element={<ActivitiesScreen />} />
-            <Route path="/car-rentals" element={<CarRentalsScreen />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-          <PWAInstallPrompt />
-        </BrowserRouter>
-      </TooltipProvider>
-    </AuthProvider>
-  </QueryClientProvider>
+  <>
+    <Toaster />
+    <Sonner />
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/accommodations" element={<AccommodationsScreen />} />
+      <Route path="/restaurants" element={<RestaurantsScreen />} />
+      <Route path="/concerts" element={<ConcertsScreen />} />
+      <Route path="/nightlife" element={<NightlifeScreen />} />
+      <Route path="/loisirs" element={<LoisirsScreen />} />
+      <Route path="/activities" element={<ActivitiesScreen />} />
+      <Route path="/car-rentals" element={<CarRentalsScreen />} />
+      {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    <PWAInstallPrompt />
+  </>
 );
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,23 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import { AuthProvider } from './contexts/AuthContext';
+import { Toaster } from './components/ui/sonner';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-createRoot(document.getElementById("root")!).render(<App />);
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Router>
+          <App />
+          <Toaster />
+        </Router>
+      </AuthProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
